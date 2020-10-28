9dde48eae39664735ed609258441e56b9d04d4e6
Jx3ri <joeri.veugelen@student.pxl.be>
2020-10-28 14:26:43 +0100

    added Sidebar style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Chat from './components/Chat'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className="app">
-      <h1>Discord clone</h1>
 
       {/* Sidebar */}
-
+      <Sidebar />
 
       {/* Chat */}
+      <Chat />
     </div>
-  );
+  )
 }
 
 export default App;
